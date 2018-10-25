a897f8d97eee574355be6811a55ac249bf675cf2
yyyyuan <gyuan1031@163.com>
2018-10-25 20:38:29 +0800

    dual_v2.2

--- data/multi_aligned_dataset.py
+++ data/multi_aligned_dataset.py
@@ -3,7 +3,7 @@ import random
 
 from PIL import Image
 
-from data.base_dataset import BaseDataset, transform_pair
+from data.base_dataset import BaseDataset, transform_fusion
 from data.image_folder import make_dataset
 
 
@@ -17,7 +17,7 @@ class MultiAlignedDataset(BaseDataset):
         self.opt = opt
         self.root = opt.dataroot
         self.dir_ABC = os.path.join(opt.dataroot, opt.phase)
-        self.ABC_paths = sorted(make_dataset(self.dir_AB))
+        self.ABC_paths = sorted(make_dataset(self.dir_ABC))
         self.alphabets = list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')
 
     def __getitem__(self, index):
