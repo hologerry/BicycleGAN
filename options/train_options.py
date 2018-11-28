f04aa0b98d378bb534d60567db3396a77ba9c40c
yyyyuan <gyuan1031@163.com>
2018-11-28 16:27:17 +0800

    increment GAN loss

--- options/train_options.py
+++ options/train_options.py
@@ -57,8 +57,8 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--lambda_L2', type=float,
                             default=10.0, help='weight for mse(B-G(A, E(B)))')
         parser.add_argument('--lambda_GAN', type=float,
-                            default=2.0, help='weight on D loss. D(G(A, E(B)))')
-        parser.add_argument('--lambda_GAN2', type=float, default=1.0,
+                            default=10.0, help='weight on D loss. D(G(A, E(B)))')
+        parser.add_argument('--lambda_GAN2', type=float, default=10.0,
                             help='weight on D2 loss, D(G(A, random_z))')
         parser.add_argument('--lambda_GAN_s2', type=float, default=20.0,
                             help='weight on ')
