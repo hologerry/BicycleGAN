ba23f3d2035692173760b9c1e11e516cbe114952
Gerry Gao <hologerry@gmail.com>
2019-02-20 11:08:35 +0800

    clean code

--- data/base_dataset.py
+++ data/base_dataset.py
@@ -45,62 +45,6 @@ def get_transform(opt):
     return transforms.Compose(transform_list)
 
 
-# def transform_pair(opt, A, B, C=None):
-#     if opt.resize_or_crop == 'resize_and_crop':
-#         A = A.resize((opt.loadSize, opt.loadSize), Image.BICUBIC)
-#         B = B.resize((opt.loadSize, opt.loadSize), Image.BICUBIC)
-#         A = transforms.ToTensor()(A)
-#         B = transforms.ToTensor()(B)
-#         w_offset = random.randint(0, max(0, opt.loadSize - opt.fineSize - 1))
-#         h_offset = random.randint(0, max(0, opt.loadSize - opt.fineSize - 1))
-
-#         A = A[:, h_offset:h_offset + opt.fineSize,
-#               w_offset:w_offset + opt.fineSize]
-#         B = B[:, h_offset:h_offset + opt.fineSize,
-#               w_offset:w_offset + opt.fineSize]
-#         A = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(A)
-#         B = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(B)
-#         new_C = []
-#         if C is not None:
-#             assert(isinstance(C, list))
-#             for one_C in C:
-#                 one_C = one_C.resize(
-#                     (opt.loadSize, opt.loadSize), Image.BICUBIC)
-#                 one_C = transforms.ToTensor()(one_C)
-#                 one_C = transforms.Normalize(
-#                     (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(one_C)
-#                 one_C = one_C[:, h_offset:h_offset +
-#                               opt.fineSize, w_offset:w_offset + opt.fineSize]
-#                 new_C.append(one_C)
-#             C = torch.cat(new_C)
-#     elif opt.resize_or_crop == 'none':
-#         A = transforms.ToTensor()(A)
-#         B = transforms.ToTensor()(B)
-#         A = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(A)
-#         B = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(B)
-#         if C is not None:
-#             assert(isinstance(C, list))
-#             C = list(map(lambda c: transforms.Normalize(
-#                 (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(transforms.ToTensor()(c)), C))
-#             C = torch.cat(C)
-#     else:
-#         raise ValueError(
-#             "For paired data, only support resize_and_crop or none --resise_or_crop mode")
-
-#     if (not opt.no_flip) and random.random() < 0.5:
-#         idx = [i for i in range(A.size(2) - 1, -1, -1)]
-#         idx = torch.LongTensor(idx)
-#         A = A.index_select(2, idx)
-#         B = B.index_select(2, idx)
-#         if C is not None:
-#             C = C.index_select(2, idx)
-
-#     if C is not None:
-#         return A, B, C
-#     else:
-#         return A, B
-
-
 def transform_multi(opt, A, B, C, Bases, Shapes, Colors, blur_Shapes, blur_Colors):
     """Transformer for multi fusion, pretrain dataset
     """
@@ -137,33 +81,6 @@ def transform_multi(opt, A, B, C, Bases, Shapes, Colors, blur_Shapes, blur_Color
     blur_Colors = torch.cat(blur_Colors)
 
     return A, B, C, Bases, Shapes, Colors, blur_Shapes, blur_Colors
-
-
-# def transform_triple(opt, A, B, C, Bases, Shapes, Colors):
-#     if not opt.resize_or_crop == 'none':
-#         raise ValueError(
-#             "Only support none mode for resize_or_crop on base_gray_color dataset")
-#     assert(isinstance(Bases, list))
-#     assert(isinstance(Shapes, list))
-#     assert(isinstance(Colors, list))
-#     A = transforms.ToTensor()(A)
-#     B = transforms.ToTensor()(B)
-#     C = transforms.ToTensor()(C)
-#     A = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(A)
-#     B = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(B)
-#     C = transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(C)
-
-#     Bases = list(map(lambda b: transforms.Normalize(
-#         (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(transforms.ToTensor()(b)), Bases))
-#     Bases = torch.cat(Bases)
-#     Shapes = list(map(lambda s: transforms.Normalize(
-#         (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(transforms.ToTensor()(s)), Shapes))
-#     Shapes = torch.cat(Shapes)
-#     Colors = list(map(lambda c: transforms.Normalize(
-#         (0.5, 0.5, 0.5), (0.5, 0.5, 0.5))(transforms.ToTensor()(c)), Colors))
-#     Colors = torch.cat(Colors)
-
-#     return A, B, C, Bases, Shapes, Colors
 
 
 def transform_few_with_label(opt, A, B, C, label, Bases, Shapes, Colors, blur_Shapes, blur_Colors):
@@ -227,12 +144,12 @@ def __scale_width(img, target_width):
 
 def transform_grid(Shapes, Colors):
     """Transform Shpaes/Colors to grid
-    0 1 2 3 ->  0 1
-                2 3
+       0 1 2 3 ->  0 1
+                   2 3
 
     Arguments:
-        Shapes {[PIL Images]} -- [4*W*H*C]
-        Colors {[PIL Images]} -- [4*W*H*C]
+        Shapes {PIL Images} -- [4*W*H*C]
+        Colors {PIL Images} -- [4*W*H*C]
 
     Returns:
         [grid tensors] -- [C*2W*2H]
