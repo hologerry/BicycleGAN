2df15dba2fd7e7704f596be0d2f08b29cab81a65
Gerry Gao <hologerry@gmail.com>
2019-05-11 17:16:11 +0800

    fix

--- options/train_options.py
+++ options/train_options.py
@@ -77,5 +77,6 @@ class TrainOptions(BaseOptions):
         parser.add_argument('--lambda_GAN2', type=float, default=1.0, help='weight on D2 loss, D(G(A, random_z))')
         parser.add_argument('--lambda_z', type=float, default=0.5, help='weight for ||E(G(random_z)) - random_z||')
         parser.add_argument('--lambda_kl', type=float, default=0.01, help='weight for KL loss')
+        parser.add_argument('--use_same_D', action='store_true', help='if two Ds share the weights or not')
         self.isTrain = True
         return parser
